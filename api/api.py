d4fedd134844f5f4cd0b024112f804d17690fa66
Mahmoud <m.e.bestawy@gmail.com>
2023-10-17 01:17:02 -0400

    Add black linter to API code

--- api/api.py
+++ api/api.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.post("/cloneRepo")
 def clone_repo(repo_link: str):
-    return {
-        "repo_link": repo_link
-    }
-
+    return {"repo_link": repo_link}
