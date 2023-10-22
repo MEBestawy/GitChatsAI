000160a3558e4ac863c4affc329138cc0603657a
Mahmoud <m.e.bestawy@gmail.com>
2023-10-22 01:10:56 -0400

    Enable API to clone github repos

--- api/api.py
+++ api/api.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from utils.utils import match_github_repo_info, clone_github_repo
 
 app = FastAPI()
 
 
-@app.post("/cloneRepo")
-def clone_repo(repo_link: str):
-    return {"repo_link": repo_link}
+@app.put("/parseGithubRepo")
+def parse_github_repo(repo_link: str):
+    SUCCESS_OUTPUT = {"result": "Success!"}
+
+    # get username and project name from url
+    match = match_github_repo_info(repo_link)
+    username, project_name = match.group("username"), match.group("project_name")
+
+    # clone project in
+    directory_path = f"{username}/{project_name}/"
+    clone_github_repo(username, project_name, directory_path)
+
+    # TODO: Parse & Embed docs from code
+
+    return SUCCESS_OUTPUT
---
+++ api/utils/utils.py
@@ -0,0 +1,43 @@
+from fastapi import HTTPException
+import re, os, subprocess
+
+
+def match_github_repo_info(repo_link: str):
+    """
+    Parses the given github repo URL (with or without the base url) and returns the Match object.
+
+    Raises a HTTPException if the provided <repo_link> could not be parsed.
+    """
+    match = re.match(
+        r"(?:https?://github\.com/)?(?P<username>[^/]+)/(?P<project_name>[^/]+)",
+        repo_link,
+    )
+    if not match:
+        raise HTTPException(
+            status_code=400,
+            detail="Could not parse github repo link. Are you sure this url links to a repo?",
+        )
+
+    return match
+
+
+def clone_github_repo(username: str, project_name: str, target_directory_path: str):
+    """
+    Clones the Github repo named <project_name> under <username>'s account into <target_directory_path>.
+    """
+    if os.path.exists(target_directory_path):
+        return
+
+    clone_command = [
+        "git",
+        "clone",
+        f"https://github.com/{username}/{project_name}",
+        target_directory_path,
+    ]
+    result = subprocess.run(clone_command, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        raise HTTPException(
+            status_code=404,
+            detail="Error cloning the repository. Are you sure this url links to a repo?",
+        )
