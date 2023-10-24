818fb75cecf71b050d805388042c31a97c11f58f
lakshaygarg0007 <lgarg_be17@thapar.edu>
2023-10-24 20:16:33 +0530

    removed headers

--- web/src/Header.jsx
+++ web/src/Header.jsx
@@ -5,7 +5,6 @@ const Header = () => {
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-                    <Link to="/models" className="mr-5 hover:text-gray-900">My Models</Link>
                     <Link to="/" className="mr-5 hover:text-gray-900">Train Model</Link>
                 </nav>
                 <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
